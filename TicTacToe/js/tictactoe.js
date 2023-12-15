a710e5fc96aa3cdf4c620bdaf4286cb5765a6879
beltoncm <beltoncb@gmail.com>
2023-12-14 21:19:53 -0500

    Pushing Tic Toe
    This is a push to keep a copy of the script onlline

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -130,3 +130,74 @@ function audio(audioURL) {
 
 
 }
+
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+
+    const canvas = document.getElementById('win-lines');
+
+    const c = canvas.getContext('2d');
+
+
+
+    let x1 = coordX1,
+
+        y1 = coordY1,
+
+        x2 = coordX2,
+
+        y2 = coordY2,
+
+        x = x1,
+    
+        y = y1;
+
+
+
+
+
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70,255,33,.8)';
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+
+        }
+
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+
+    }
+    
+
+    function clear() {
+    
+        const animationLoop = requestAnimationFrame(clear);
+
+        c.clearRect(0, 0, 608, 608);
+
+        cancelAnimationFrame(animationLoop);
+    }
+
+        disableClick();
+        audio('./media/winGame.mp3');
+
+        animateLineDrawing();
+
+        setTimeout(function () { clear(); resetGame(); }, 1000);
+
+
+
+    }
+
+
